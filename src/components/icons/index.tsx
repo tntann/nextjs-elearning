3e62db892832f7e8965e1a83d79e4457d92a54e3
tntan <nhattan8421@gmail.com>
2024-10-09 16:44:59 +0700

    setup routing for the project nextjs elearning

--- src/components/icons/index.tsx
+++ src/components/icons/index.tsx
@@ -1,3 +1,7 @@
 export { default as IconPlay } from "./IconPlay";
 export { default as IconExplore } from "./IconExplore";
 export { default as IconStudy } from "./IconStudy";
+export { default as IconOrder } from "./IconOrder";
+export { default as IconComment } from "./IconComment";
+export { default as IconUsers } from "./IconUsers";
+export { default as IconCourse } from "./IconCourse";
--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -1,14 +1,41 @@
-import { IconStudy, IconExplore } from "@/components/icons";
+import {
+  IconStudy,
+  IconUsers,
+  IconOrder,
+  IconComment,
+  IconCourse,
+  IconExplore,
+} from "@/components/icons";
 import { TMenuItem } from "@/types";
 export const menuItems: TMenuItem[] = [
   {
     url: "/",
+    title: "Khám phá",
+    icon: <IconExplore className="size-5" />,
+  },
+  {
+    url: "/study",
     title: "Khu vực học tập",
     icon: <IconStudy className="size-5" />,
   },
   {
-    url: "/explore",
-    title: "Khám phá",
-    icon: <IconExplore className="size-5" />,
+    url: "/manage/course",
+    title: "Quản lý khoá học",
+    icon: <IconCourse className="size-5" />,
+  },
+  {
+    url: "/manage/member",
+    title: "Quản lý thành viên",
+    icon: <IconUsers className="size-5" />,
+  },
+  {
+    url: "/manage/order",
+    title: "Quản lý đơn hàng",
+    icon: <IconOrder className="size-5" />,
+  },
+  {
+    url: "/manage/comment",
+    title: "Quản lý bình luận",
+    icon: <IconComment className="size-5" />,
   },
 ];
